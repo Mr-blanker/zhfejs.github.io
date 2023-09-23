fef32d804027be2d65115c003c1829c5dd9f4784
zhenghui <ffzhenghui3@chinatelecom.cn>
2023-09-23 16:55:11 +0800

    feat: 增加docs

---
+++ .vuepress/config.js
@@ -0,0 +1,11 @@
+/*
+ * @Description: 
+ * @Autor: zhenghui
+ * @Date: 2023-09-23 16:41:37
+ */
+module.exports = {
+    theme: '@vuepress/blog',
+    themeConfig: {
+      // 请参考文档来查看所有可用的选项。
+    }
+  }
---
+++ docs/README.md
@@ -0,0 +1,6 @@
+<!--
+ * @Description: 
+ * @Autor: zhenghui
+ * @Date: 2023-09-23 16:37:30
+-->
+# Hello world
